b3b8e0a2893590b686a32e027700852c079ca6b9
Lucas <lucasdesriaud6675@gmail.com>
2023-05-16 15:08:49 +0200

    modif nom image

--- saeBataille/src/application/Main.java
+++ saeBataille/src/application/Main.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * Classe qui va lancer l'application de jeu.
@@ -16,11 +19,19 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader chargeurFXML = new FXMLLoader();
-			System.out.println("view folder: " + Main.class.getResource("vue/MenuPrincipal.fxml"));     // returns null
-			chargeurFXML.setLocation(getClass().getResource("vue/MenuPrincipal.fxml"));
+			System.out.println("view folder: " + Main.class.getResource("saeBataille/src/application/vue/image/porteAvions.png"));     
+			chargeurFXML.setLocation(getClass().getResource("saeBataille\\src\\application\\vue\\image\\porteAvions.png"));
 			
 			Parent racine = chargeurFXML.load();
-			
+	        // Charger une image à partir d'un fichier
+	        Image image = new Image("vue/image/porteAvions.png");
+
+	        // Obtenir la référence à votre ImageView à partir du fichier FXML
+	        ImageView imageView = (ImageView) chargeurFXML.getNamespace().get("porteAvions");
+
+	        // Assigner l'image à l'ImageView
+	        imageView.setImage(image);
+	        
 			Scene scene = new Scene(racine);
 //			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
