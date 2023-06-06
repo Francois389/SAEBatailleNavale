021b9f9360cbaaa97a3e1fd1d60e60b33cdca6a6
cleeem <sp2.clem@gmail.com>
2023-06-06 16:17:38 +0200

    Update ControllerTransition.java

--- saeBataille/src/application/controller/ControllerTransition.java
+++ saeBataille/src/application/controller/ControllerTransition.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import jeu.partie.Joueur;
 
-public class ControllerTransition {
+public class controllerTransition {
 
 	@FXML
 	private Label pseudoJoueur1;
@@ -19,36 +19,32 @@ public class ControllerTransition {
 	private Boolean bateauJ2place = false ;
 	
 	
-	protected Joueur Joueur1 =  Modele.getPartieEnCours().getJoueur1(); 
-	protected Joueur Joueur2 =  Modele.getPartieEnCours().getJoueur2(); 
+	private Joueur Joueur1 =  Modele.getPartieEnCours().getJoueur1(); 
+	private Joueur Joueur2 =  Modele.getPartieEnCours().getJoueur2(); 
 	
-
-	private static ControllerTransition controllerCourant;
+	// Par defaut le joueur 1  commencee
+	private Joueur joueurSuivant = Joueur1;
 	
-	@FXML
-	void initialize() {
-		ControllerTransition.controllerCourant =  this;
-	}
 	@FXML
 	private void tourSuivant() {
-		Modele.getPartieEnCours().incrementNbTour();
-		System.out.println(Modele.getPartieEnCours().getNbTour());
-		//System.out.println(Joueur1 + " " + Joueur2);
+		System.out.println(Joueur1 + " " + Joueur2);
 		if (!bateauJ2place) {
 			bateauJ2place = true ;
 			Main.activerPositionBateau2();
-			//joueurSuivant(Joueur2 , Joueur1);
+			joueurSuivant(Joueur2 , Joueur1);
 		} else {
-			Main.activerPageDeJeux();
-			
-			// TODO changer pour ecran jeu joueur2
+			if (Modele.getPartieEnCours().getAutreJoueur().equals(Joueur1))  {
+				Main a = new Main();
+       			a.chargementPageDependante();
+       			Main.activerPageDeJeux();
+			}
 		}
-		
 	}
 	
-	public static void affichage() {
-		
-		controllerCourant.pseudoJoueur1.setText(Modele.getPartieEnCours().getJoueurActuel().getNom());
-		controllerCourant.pseudoJoueur2.setText(Modele.getPartieEnCours().getAutreJoueur().getNom());
+	private void joueurSuivant(Joueur suivant , Joueur actuel) {
+		pseudoJoueur1.setText(actuel.getNom());
+		pseudoJoueur2.setText(suivant.getNom());
+		System.out.println("pseudo changer");
+	
 	}
 }
