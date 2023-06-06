78454895af308e18ab9c52f7962e4aee2592c55c
cleeem <sp2.clem@gmail.com>
2023-06-06 17:45:13 +0200

    point virgule

--- saeBataille/src/application/controller/PageDeJeuxControlleur.java
+++ saeBataille/src/application/controller/PageDeJeuxControlleur.java
@@ -229,7 +229,7 @@ public class PageDeJeuxControlleur {
 			alert.showAndWait();
 			
 			Main a = new Main();
-        	a.chargementTranstion();;     
+        	a.chargementTranstion();     
         	Main.activerEcranTransition(); 
 
 		}
