26f27748d4f4f0e4bd1aaedc0e5ebcb891807faf
EgtXenos <129937761+EgtXenos@users.noreply.github.com>
2023-06-06 09:23:06 +0200

    controller transition

--- saeBataille/src/application/controller/controllerTransition.java
+++ saeBataille/src/application/controller/controllerTransition.java
@@ -27,15 +27,29 @@ public class controllerTransition {
 	
 	@FXML
 	private void tourSuivant() {
+		System.out.println(Joueur1 + " " + Joueur2);
 		if (!bateauJ2place) {
+			bateauJ2place = true ;
 			Main.activerPositionBateau2();
-		} else if (joueurSuivant.equals(Joueur1)){
+			joueurSuivant(Joueur2 , Joueur1);
+		} else if (joueurSuivant.equals(Joueur1)) {
+			joueurSuivant(Joueur1 , Joueur2);
 			joueurSuivant = Joueur2;
 			// TODO changer pour ecran jeu joueur1
 		} else if (joueurSuivant.equals(Joueur2)) {
+			joueurSuivant(Joueur2 , Joueur1);
 			joueurSuivant = Joueur1;
+			
+			
 			// TODO changer pour ecran jeu joueur2
 		}
 		
+	}
+	
+	private void joueurSuivant(Joueur suivant , Joueur actuel) {
+		pseudoJoueur1.setText(actuel.getNom());
+		pseudoJoueur2.setText(suivant.getNom());
+		System.out.println("pseudo changer");
+	
 	}
 }
